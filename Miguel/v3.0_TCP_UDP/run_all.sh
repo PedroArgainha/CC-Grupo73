f615ee09a7f49f9927239c7bcf0aeba831e3dd7d
Pedro Argainha <pedro.argainha@gmail.com>
2025-12-09 02:47:49 +0000

    add script para 3 rovers 2

--- Miguel/v3.0_TCP_UDP/run_all.sh
+++ Miguel/v3.0_TCP_UDP/run_all.sh
@@ -1,57 +1,60 @@
 #!/bin/bash
 
+# Caminho para a pasta do teu projeto
 PROJ="/home/core/CC-Grupo73/Miguel/v3_0_TCP_UDP"
 
-# =======================
-#  NAVE-MÃE
-# =======================
-gnome-terminal --title="NaveMae" -- bash -c "
-core-term navemae -- bash -c '
-cd $PROJ
-python3 navemae.py --host 10.0.0.10 --port 6000
-'
-"
+echo "A iniciar sistema..."
 
-# =======================
-#  ROVER 1
-# =======================
-gnome-terminal --title="Rover1" -- bash -c "
-core-term rover1 -- bash -c '
-cd $PROJ
-python3 roverAPI.py --id 1 --host 10.0.0.10 --port 6000 --vel 1 --tick 0.5
-'
-"
+##############################
+# NAVE-MÃE
+##############################
+xterm -T "NaveMae" -e "
+core-term navemae -- bash -lc '
+cd $PROJ;
+python3 navemae.py --host 10.0.0.10 --port 6000;
+exec bash'
+" &
 
-# =======================
-#  ROVER 2
-# =======================
-gnome-terminal --title="Rover2" -- bash -c "
-core-term rover2 -- bash -c '
-cd $PROJ
-python3 roverAPI.py --id 2 --host 10.0.0.10 --port 6000 --vel 1 --tick 0.5
-'
-"
+##############################
+# ROVER 1
+##############################
+xterm -T "Rover1" -e "
+core-term rover1 -- bash -lc '
+cd $PROJ;
+python3 roverAPI.py --id 1 --host 10.0.0.10 --port 6000 --vel 1 --tick 0.5;
+exec bash'
+" &
 
-# =======================
-#  ROVER 3
-# =======================
-gnome-terminal --title="Rover3" -- bash -c "
-core-term rover3 -- bash -c '
-cd $PROJ
-python3 roverAPI.py --id 3 --host 10.0.0.10 --port 6000 --vel 1 --tick 0.5
-'
-"
+##############################
+# ROVER 2
+##############################
+xterm -T "Rover2" -e "
+core-term rover2 -- bash -lc '
+cd $PROJ;
+python3 roverAPI.py --id 2 --host 10.0.0.10 --port 6000 --vel 1 --tick 0.5;
+exec bash'
+" &
 
-# =======================
-#  GROUND CONTROL
-# =======================
-gnome-terminal --title="GC" -- bash -c "
-core-term gc -- bash -c '
-cd $PROJ
-python3 groundControl.py --host 10.0.0.10 --port 2900
-'
-"
+##############################
+# ROVER 3
+##############################
+xterm -T "Rover3" -e "
+core-term rover3 -- bash -lc '
+cd $PROJ;
+python3 roverAPI.py --id 3 --host 10.0.0.10 --port 6000 --vel 1 --tick 0.5;
+exec bash'
+" &
 
-echo "====================="
-echo "Sistema arrancado!"
-echo "====================="
+##############################
+# GROUND CONTROL
+##############################
+xterm -T "GC" -e "
+core-term gc -- bash -lc '
+cd $PROJ;
+python3 groundControl.py --host 10.0.0.10 --port 2900;
+exec bash'
+" &
+
+echo "=================================="
+echo "      Sistema arrancado! "
+echo "=================================="
